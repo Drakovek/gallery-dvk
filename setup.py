1c79555e6eb555221bc7602587ddddefed58c45d
Drakovek <DrakovekMail@gmail.com>
2023-08-13 11:32:50 -0700

    Added Extractor for Transfur.com.

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""Setuptools setup file."""
+
+import setuptools
+
+console_scripts = ["gallery-dvk = gallery_dvk.gallery_dvk:main"]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gallery-dvk",
+    version="0.0.1",
+    author="Drakovek",
+    author_email="DrakovekMail@gmail.com",
+    description="Utility for downloading media from various image hosting websites.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Drakovek/gallery-dvk",
+    packages=setuptools.find_packages(),
+    install_requires=["beautifulsoup4",
+                "HTML-String-Tools",
+                "Metadata-Magic",
+                "Python-Print-Tools",
+                "requests",
+                "tqdm"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.0',
+    entry_points={"console_scripts": console_scripts}
+)
